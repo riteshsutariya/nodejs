2d664f00f5fdc20ad64131c282567023e1908aed
riteshsutariya <riteshsutariya94@gmail.com>
2022-06-10 17:30:40 +0530

    other global objects in nodeJs

---
+++ globals/otherGlobals.js
@@ -0,0 +1,11 @@
+console.log("current excuting file directory name: "+__dirname);
+console.log("current executing file name: "+__filename);
+
+console.log(this);
+
+//setInterval(),clearInterval(),setTimeOut() and clearTimeOut() are also part of globals
+
+//console is also global object
+console.log(console);
+
+//there are other global objects also.
